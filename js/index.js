ae467b8457cfd21c36dc9da59317f62f0e0c99de
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-05-13 18:28:34 -0400

    Completed the assignment

--- js/index.js
+++ js/index.js
@@ -40,3 +40,75 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navItems = document.querySelectorAll('header nav a');
+let navBar = document.querySelector('nav');
+let homeItem = document.createElement('a');
+let dontClickItem = document.createElement('a');
+dontClickItem.style.color = 'green';
+
+homeItem.textContent = 'Home';
+homeItem.style.color = 'green';
+navItems[0].textContent = 'Services';
+navItems[1].textContent = 'Product';
+navItems[2].textContent = 'Vision';
+navItems[3].textContent = 'Features';
+navItems[4].textContent = 'About';
+navItems[5].textContent = 'Contact';
+dontClickItem.textContent = 'Dont Click';
+navBar.appendChild(dontClickItem);
+navBar.prepend(homeItem);
+
+navItems.forEach(function (ele) {
+  ele.style.color = 'green';
+})
+
+let h1Item = document.querySelector('h1');
+// let br = document.createElement('BR');
+// h1Item.textContent = 'DOM ' + document.write('<br> /') + 'IS AWESOME';
+h1Item.innerHTML = 'DOM <br /> IS <br /> AWESOME';
+let buttonOne = document.querySelector('button');
+buttonOne.textContent = 'Get Started';
+
+let headerIMG = document.getElementById('cta-img');
+headerIMG.src = 'img/header-img.png';
+
+let mainContent = document.querySelectorAll('.text-content');
+mainContent[0].firstChild.textContent = 'Features';
+mainContent[0].lastChild.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+mainContent[1].firstChild.textContent = 'About';
+mainContent[1].lastChild.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+mainContent[2].firstChild.textContent = 'Services';
+mainContent[2].lastChild.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+mainContent[3].firstChild.textContent = 'Product';
+mainContent[3].lastChild.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+mainContent[4].firstChild.textContent = 'Vision';
+mainContent[4].lastChild.textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+let mainIMG = document.getElementById('middle-img');
+mainIMG.src = 'img/mid-page-accent.jpg';
+
+let contactInfo = document.querySelector('.contact');
+let contactH4 = document.createElement('h4');
+contactH4.textContent = 'Contact';
+let contactAddress = document.createElement('p');
+contactAddress.innerHTML = '123 Way 456 Street, <br /> Somewhere, USA';
+let contactNumber = document.createElement('p');
+contactNumber.textContent = '1 (888) 888-8888';
+let contactEmail = document.createElement('p');
+contactEmail.textContent = 'sales@greatidea.io';
+
+contactInfo.appendChild(contactH4);
+contactInfo.appendChild(contactAddress);
+contactInfo.appendChild(contactNumber);
+contactInfo.appendChild(contactEmail);
+
+let footerItem = document.createElement('footer');
+document.body.append(footerItem);
+let footerP = document.createElement('p');
+footerP.textContent = 'Copyright Great Idea! 2018';
+footerItem.appendChild(footerP);
